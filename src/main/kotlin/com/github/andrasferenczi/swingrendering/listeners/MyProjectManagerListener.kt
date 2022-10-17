8cccb55fe094b0bd395b6bf3baf21c255ba0c275
GitHub Action <action@github.com>
2022-10-17 15:25:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/andrasferenczi/swingrendering/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/andrasferenczi/swingrendering/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.andrasferenczi.swingrendering.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.andrasferenczi.swingrendering.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/andrasferenczi/swingrendering/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/andrasferenczi/swingrendering/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.andrasferenczi.swingrendering.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.andrasferenczi.swingrendering.MyBundle
 
 class MyApplicationService {
 
